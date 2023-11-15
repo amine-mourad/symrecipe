7f266c371a98fcbff8a39b0cf7393333163a4969
mourad amine <mouradwork1@gmail.com>
2023-11-15 20:43:15 +0100

    add assert and fixture to project

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create("fr_FR");
+
+        for ($i = 0; $i < 50; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($faker->name);
+            $ingredient->setPrice($faker->randomFloat(2, 0, 200));
+            $manager->persist($ingredient);
+        }
+
+        $manager->flush();
+    }
+}
